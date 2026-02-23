236231d6c0dad9762f9778eab9b0e99f157b7cd2
Ashutosh Kashyap <ashutoshkashyap121@gmail.com>
2026-02-23 10:19:16 +0530

    Fix prisma named export for production build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,19 +4,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
+export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
-export default prisma;
