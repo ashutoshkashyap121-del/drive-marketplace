4951320d075b006bb820eaad6a588915cb7ea383
Ashutosh Kashyap <ashutoshkashyap121@gmail.com>
2026-03-09 15:25:08 +0530

    fix: trainer page and light theme homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,6 +150,9 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+<link href="https://fonts.googleapis.com/css2?family=Sora:wght@700;800&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
